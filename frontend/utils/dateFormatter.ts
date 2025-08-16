958be310e7174fe69cb08e379d047c4cfbfd6ff4
mitchellquitz <mitchellquitz@gmail.com>
2025-08-16 12:27:54 +0200

    add dateformat util

---
+++ frontend/utils/dateFormatter.ts
@@ -0,0 +1,13 @@
+export const formatDate = (
+  date: Date | string, 
+  options?: Intl.DateTimeFormatOptions
+): string => {
+  const dateObj = typeof date === 'string' ? new Date(date) : date;
+  
+  return dateObj.toLocaleDateString('en-EN', options || {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  });
+};
