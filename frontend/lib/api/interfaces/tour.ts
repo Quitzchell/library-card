2d21d27c393267ae2a1e5f2079c56975a5610da1
mitchellquitz <mitchellquitz@gmail.com>
2025-08-17 02:35:42 +0200

    create interface / entity for TourDate

---
+++ frontend/lib/api/interfaces/tour.ts
@@ -0,0 +1,24 @@
+export interface TourDate {
+  id: number;
+  date: Date;
+  venue: string;
+  city: string;
+  country: string;
+  ticket_url?: string;
+  sold_out?: boolean;
+  description?: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export type TourDateDisplay = Omit<TourDate, "created_at" | "updated_at">;
+
+export interface TourResponse {
+  data: TourDateDisplay[];
+  meta?: {
+    current_page: number;
+    last_page: number;
+    per_page: number;
+    total: number;
+  };
+}
