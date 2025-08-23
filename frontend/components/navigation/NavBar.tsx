5694f566f9b75e196af86eb34afeb67fbae91f5e
mitchellquitz <mitchellquitz@gmail.com>
2025-08-23 17:14:37 +0200

    empty navBar for now, focus on mobile first

--- frontend/components/navigation/NavBar.tsx
+++ frontend/components/navigation/NavBar.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 import { cn } from "@/utils/styling";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
 import { navItems } from "./navigationConstants";
 
 interface NavBarProps {
@@ -15,30 +9,6 @@ interface NavBarProps {
 
 export default function NavBar({ pathname }: NavBarProps) {
   return (
-    <NavigationMenu className={cn("w-full max-w-none justify-between")}>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/" className="hover:line-through">
-            <h2 className="text-4xl font-bold">Library Card</h2>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-
-      <NavigationMenuList>
-        {navItems.map((item) => (
-          <NavigationMenuItem key={item.name}>
-            <NavigationMenuLink
-              href={item.href}
-              className={cn(
-                "text-lg",
-                pathname.endsWith(item.href) ? "font-semibold" : "",
-              )}
-            >
-              {item.name}
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+    <></>
   );
 }
