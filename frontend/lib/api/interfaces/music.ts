f61780ce0ed196731294ea15aed113809fdc147b
mitchellquitz <mitchellquitz@gmail.com>
2025-09-01 22:48:23 +0200

    update music.ts

--- frontend/lib/api/interfaces/music.ts
+++ frontend/lib/api/interfaces/music.ts
@@ -1,18 +1,16 @@
+export type ServicesVariant = { name: string; url: string };
+
 export interface MusicItem {
   id: number;
   title: string;
   release_date?: string;
-  cover_image?: string;
-  spotify_url?: string;
-  bandcamp_item?: BandCampItem;
-  apple_music_url?: string;
+  cover_image: string;
+  services: Array<ServicesVariant>;
   created_at: string;
   updated_at: string;
 }
 
-export type BandCampItem = { type: string; id: string }
-
-export type MusicItemDisplay = Omit<MusicItem, "created_at" | "updated_at">
+export type MusicItemDisplay = Omit<MusicItem, "created_at" | "updated_at">;
 
 export interface MusicItemResponse {
   data: MusicItemDisplay[];
@@ -23,5 +21,3 @@ export interface MusicItemResponse {
     total: number;
   };
 }
-
-// https://thisislibrarycard.bandcamp.com/track/art-school
