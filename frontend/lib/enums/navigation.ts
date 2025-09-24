b341ef727573edcab5b8688f4f24698240b2d078
mitchellquitz <mitchellquitz@gmail.com>
2025-09-24 21:51:23 +0200

    update navigation enum

--- frontend/lib/enums/navigation.ts
+++ frontend/lib/enums/navigation.ts
@@ -12,6 +12,19 @@ export enum NavigationLabel {
   TOUR = "Tour",
   MUSIC = "Music",
   VIDEO = "Video",
-  ABOUT= "About",
+  ABOUT = "About",
   CONTACT = "Contact",
 }
+
+export const navItems = [
+  { name: NavigationLabel.TOUR, href: NavigationRoute.TOUR },
+  { name: NavigationLabel.MUSIC, href: NavigationRoute.MUSIC },
+  { name: NavigationLabel.VIDEO, href: NavigationRoute.VIDEO },
+  { name: NavigationLabel.ABOUT, href: NavigationRoute.ABOUT },
+  { name: NavigationLabel.CONTACT, href: NavigationRoute.CONTACT },
+];
+
+export const homeItem = {
+  name: NavigationLabel.HOME,
+  href: NavigationRoute.HOME,
+};
