2acda4145bb11d0bc00e5de0417863feda534094
mitchellquitz <mitchellquitz@gmail.com>
2025-10-31 10:11:03 +0100

    initial version for touring page

--- frontend/app/tour/page.tsx
+++ frontend/app/tour/page.tsx
@@ -1,12 +1,36 @@
+"use client";
+
+import TourList from "@/components/homepage/tour/components/TourList";
+import { services } from "@/lib/services.config";
+import { useEffect, useState } from "react";
+import { TourResponse } from "@/lib/interfaces/tour";
+
 export default function TourPage() {
+  const [tourDates, setTourDates] = useState<TourResponse>({ data: [] });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchTourDates() {
+      try {
+        const response = await services.tour.getTourDates(0, 20);
+        setTourDates(response);
+      } catch (error) {
+        console.error("Error fetching tourdates:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchTourDates();
+  }, []);
+
   return (
-    <div className="min-h-screen">
-      <main className="container mx-auto px-4 py-8">
-        <h1 className="font-playfair mb-8 text-4xl font-bold">Tour</h1>
-        <p className="text-lg text-gray-600">
-          Upcoming tour dates and locations.
-        </p>
-      </main>
+    <div className="container min-h-screen px-4 py-8">
+      <h1 className="font-playfair mb-8 text-4xl font-bold">Tour</h1>
+      {tourDates.data.length > 0 ? (
+        <TourList tourDates={tourDates} />
+      ) : (
+        <p>No tourdates found</p>
+      )}
     </div>
   );
 }
