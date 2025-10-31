107e70fa9fc137d7a32b0a1bd044a61cc4a1f5d8
mitchellquitz <mitchellquitz@gmail.com>
2025-10-31 10:11:14 +0100

    refactor tourList component

--- frontend/components/homepage/tour/components/TourList.tsx
+++ frontend/components/homepage/tour/components/TourList.tsx
@@ -5,30 +5,12 @@ import { Button } from "../../../ui/button";
 import { TourDateDisplay, TourResponse } from "@/lib/interfaces/tour";
 import { cn } from "@/utils/classnames";
 
-const getBorderClasses = (idx: number, midpoint: number) => {
-  const isFirstRow = idx === 0;
-  const isMidpoint = midpoint === idx;
-  return cn(
-    "border-b border-black",
-    (isFirstRow || isMidpoint) && "md:border-t",
-    isFirstRow && "border-t",
-  );
-};
-
 export default function TourList({ tourDates }: { tourDates: TourResponse }) {
-  const midpoint = tourDates.data.length / 2 - 1;
-
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 md:gap-x-5">
-      {tourDates.data.map((tourDate, idx) => (
+    <section className="grid grid-cols-1 divide-y border-y md:grid-cols-2 md:gap-x-5">
+      {tourDates.data.map((tourDate) => (
         <div className="grid grid-cols-12 gap-x-5" key={tourDate.id}>
-          <Button
-            variant="ghost"
-            className={cn(
-              "col-span-7 text-left",
-              getBorderClasses(idx, midpoint),
-            )}
-          >
+          <Button variant="ghost" className="col-span-7 text-left">
             <div>
               <p className="px-2 font-bold">
                 {formatDate(tourDate.date, {
@@ -38,70 +20,47 @@ export default function TourList({ tourDates }: { tourDates: TourResponse }) {
                 })}
               </p>
               <p className="px-2 text-balance">{tourDate.venue}</p>
-              <p className="mt-2 px-2 text-sm text-balance">
+              <p className="mt-1 px-2 text-sm text-balance">
                 {tourDate.city}, {tourDate.country}
               </p>
             </div>
           </Button>
 
-          <TicketButtonContent
-            tourDate={tourDate}
-            idx={idx}
-            midpoint={midpoint}
-          />
+          <TicketButtonContent tourDate={tourDate} />
         </div>
       ))}
     </section>
   );
 }
 
-function TicketButtonContent({
-  tourDate,
-  idx,
-  midpoint,
-}: {
-  tourDate: TourDateDisplay;
-  idx: number;
-  midpoint: number;
-}) {
+function TicketButtonContent({ tourDate }: { tourDate: TourDateDisplay }) {
   if (tourDate.sold_out) {
     return (
-      <div
-        className={cn(
-          "col-span-5 flex justify-end p-2",
-          getBorderClasses(idx, midpoint),
-        )}
-      >
-        <p className="line-through">Sold out</p>
+      <div className="col-span-5 flex justify-end">
+        <div className={cn("h-fit w-fit p-2")}>
+          <p className="line-through">Sold out</p>
+        </div>
       </div>
     );
   }
 
   if (tourDate.ticket_url) {
     return (
-      <Button
-        asChild
-        variant="ghost"
-        className={cn(
-          "col-span-5 flex justify-end p-2",
-          getBorderClasses(idx, midpoint),
-        )}
-      >
-        <Link href={tourDate.ticket_url} className="h-full w-full text-end">
-          <p className="underline">Get tickets</p>
-        </Link>
-      </Button>
+      <div className="col-span-5 flex justify-end">
+        <Button asChild variant="ghost" className="h-fit w-fit p-2">
+          <Link href={tourDate.ticket_url} className="text-end">
+            <p className="underline">Get tickets</p>
+          </Link>
+        </Button>
+      </div>
     );
   }
 
   return (
-    <div
-      className={cn(
-        "col-span-5 flex justify-end p-2",
-        getBorderClasses(idx, midpoint),
-      )}
-    >
-      <p>Free event</p>
+    <div className="col-span-5 flex justify-end">
+      <div className={cn("h-fit w-fit p-2")}>
+        <p>Free event</p>
+      </div>
     </div>
   );
 }
