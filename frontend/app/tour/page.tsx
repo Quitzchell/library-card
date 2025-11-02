0004db4b530527028d1ba473ab8dce4b27b00698
mitchellquitz <mitchellquitz@gmail.com>
2025-11-02 20:09:55 +0100

    refactor container utility

--- frontend/app/tour/page.tsx
+++ frontend/app/tour/page.tsx
@@ -3,7 +3,7 @@ import { TourDateEnum } from "@/lib/enums/tour";
 
 export default function TourPage() {
   return (
-    <div className="container flex grow-1 flex-col px-4 py-8">
+    <div className="container flex grow-1 flex-col py-8">
       <h1 className="font-playfair mb-8 text-4xl font-bold">Tour</h1>
       <section className="h-full space-y-12">
         <h2 className="font-playfair mb-8 text-2xl font-bold">
