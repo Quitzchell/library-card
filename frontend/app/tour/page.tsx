2539871c681abd7a82072d1a24a38d98476aa3e9
mitchellquitz <mitchellquitz@gmail.com>
2025-11-08 22:20:58 +0100

    refactor tour components

--- frontend/app/tour/page.tsx
+++ frontend/app/tour/page.tsx
@@ -4,15 +4,13 @@ import { TourDateEnum } from "@/lib/enums/tour";
 export default function TourPage() {
   return (
     <div className="container flex grow-1 flex-col py-8">
-      <h1 className="font-playfair mb-8 text-4xl font-bold">Tour</h1>
+      <h1 className="mb-8 text-4xl font-bold">Tour</h1>
+
       <section className="h-full space-y-12">
-        <h2 className="font-playfair mb-8 text-2xl font-bold">
-          Upcoming Shows
-        </h2>
+        <h2 className="mb-8 text-2xl font-bold">Upcoming Shows</h2>
         <TourContainer direction={TourDateEnum.UPCOMING} />
-        <h2 className="font-playfair mb-8 text-2xl font-bold">
-          Previous Shows
-        </h2>
+
+        <h2 className="mb-8 text-2xl font-bold">Previous Shows</h2>
         <TourContainer direction={TourDateEnum.PAST} />
       </section>
     </div>
--- frontend/components/tour/TourContainer.tsx
+++ frontend/components/tour/TourContainer.tsx
@@ -6,28 +6,29 @@ import { services } from "@/lib/services.config";
 import TourList from "./TourList";
 import PaginationContainer from "../common/Pagination";
 import { TourDateEnum } from "@/lib/enums/tour";
+import { usePagination } from "@/hooks/usePagination";
+
+type TourContainerProps = {
+  direction: TourDateEnum;
+
+}
 
 const PER_PAGE = 4;
 
 export default function TourContainer({
   direction,
-}: {
-  direction: TourDateEnum;
-}) {
+}: TourContainerProps) {
   const [tourDates, setTourDates] = useState<TourResponse>({
     data: [],
     meta: { current_page: 1, total_pages: 1, per_page: 0, total: 1 },
   });
-  const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   const { total_pages } = tourDates.meta;
 
-  const handlePrev = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
-  const handleNext = () =>
-    setCurrentPage((prev) => Math.min(prev + 1, total_pages));
-  const handleNumber = (i: number) => setCurrentPage(i + 1);
+  const { currentPage, handleNext, handlePrev, handleNumber } =
+    usePagination(total_pages);
 
   useEffect(() => {
     const fetchTourDates = async () => {
@@ -41,7 +42,7 @@ export default function TourContainer({
         );
         setTourDates(response);
       } catch (err) {
-        console.error("Error fetching tour dates:", err);
+        console.error("Error fetching tour dates: ", err);
         setError("Failed to load tour dates. Please try again.");
       } finally {
         setLoading(false);
