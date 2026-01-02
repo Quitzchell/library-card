1c0686923cd1578069429e0b2f03acc9fe57a892
mitchellquitz <mitchellquitz@gmail.com>
2026-01-02 11:20:38 +0100

    initial setup for videos page

--- frontend/components/video/VideoList.tsx
+++ frontend/components/video/VideoList.tsx
@@ -1,5 +1,8 @@
 import { VideoCategory } from "@/lib/enums/video";
 import { VideoItemDisplay } from "@/lib/interfaces/video";
+import VideoItem from "./VideoItem";
+
+const PER_ROW = 2;
 
 export default function VideoList({
   category,
@@ -8,13 +11,25 @@ export default function VideoList({
   category: VideoCategory;
   videos: VideoItemDisplay[];
 }) {
+  const emptySlots = Math.max(0, PER_ROW % videos.length);
+  console.log(emptySlots)
+
   return (
-    <section>
+    <section className="space-y-4">
       <h2 className="text-xl font-semibold">{category}</h2>
 
-      <ul className="space-y-2">
-        {videos.map((video) => (
-          <li key={video.id}>{video.title}</li>
+      <ul className="grid md:grid-cols-2 gap-4">
+        {videos.map((video: VideoItemDisplay, index: number) => (
+          <li key={index}>
+            <VideoItem videoItem={video} />
+          </li>
+        ))}
+
+        {Array.from({ length: emptySlots }).map((_, i) => (
+          <div
+            key={`empty-${i}`}
+            className="h-full w-full bg-black hidden md:block"
+          />
         ))}
       </ul>
     </section>
