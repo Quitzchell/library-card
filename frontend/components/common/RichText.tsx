8448f245dec09a6495ef61d64ec6edf37fd69efd
mitchellquitz <mitchellquitz@gmail.com>
2026-01-14 21:35:10 +0100

    restyle RichText component

--- frontend/components/common/RichText.tsx
+++ frontend/components/common/RichText.tsx
@@ -10,7 +10,7 @@ export default function RichText({
   return (
     <div className="flex flex-col space-y-4">
       {richTextItem.title && (
-        <h3 className="text-2xl font-bold">{richTextItem.title}</h3>
+        <h3 className="text-2xl">{richTextItem.title}</h3>
       )}
       <div
         className="prose prose-sm max-w-none space-y-4"
