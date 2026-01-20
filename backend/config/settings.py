ecdb72e1daf433697965feececefbfeb85260cbe
mitchellquitz <mitchellquitz@gmail.com>
2026-01-20 21:30:21 +0100

    update media_root

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv(
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -130,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Media files (user uploads)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- backend/music/models/release.py
+++ backend/music/models/release.py
@@ -4,7 +4,7 @@ from .streaming_service import StreamingService
 class Release(models.Model):
     title = models.CharField(max_length=255)
     release_date = models.DateField()
-    cover_image = models.ImageField(upload_to='covers/')
+    cover_image = models.ImageField(upload_to='release-covers/')
     services = models.ManyToManyField(StreamingService)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
