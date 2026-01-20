1fcebe510feaa6a4ab2066b894e1fe8495ca5bf3
mitchellquitz <mitchellquitz@gmail.com>
2026-01-20 21:30:47 +0100

    create object managers

--- backend/music/admin.py
+++ backend/music/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models.release import Release
+from .models.streaming_service import StreamingService
+
 # Register your models here.
+admin.site.register(Release)
+admin.site.register(StreamingService)
--- backend/tour/admin.py
+++ backend/tour/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models.tour_date import TourDate
+
 # Register your models here.
+admin.site.register(TourDate)
