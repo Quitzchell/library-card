eb09c341e6f76a51a5acb254aac1b5328292688a
mitchellquitz <mitchellquitz@gmail.com>
2026-02-20 11:26:35 +0100

    update styling for release placement

--- frontend/app/music/page.tsx
+++ frontend/app/music/page.tsx
@@ -12,7 +12,7 @@ export default async function MusicPage({ searchParams }: MusicPageProps) {
     <div className="container flex grow-1 flex-col py-8">
       <h1 className="mb-8 text-4xl font-bold">Music</h1>
 
-      <section className="h-full space-y-12">
+      <section className="h-full content-center space-y-12">
         <ReleaseSection page={page} />
       </section>
     </div>
