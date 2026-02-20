ef328cc32e1145f666b37d599061dade3b3a96fa
mitchellquitz <mitchellquitz@gmail.com>
2026-02-20 10:59:55 +0100

    format

--- frontend/components/common/RichText.tsx
+++ frontend/components/common/RichText.tsx
@@ -9,9 +9,7 @@ export default function RichText({
 }: RichTextProps) {
   return (
     <div className="flex flex-col space-y-4">
-      {richTextItem.title && (
-        <h3 className="text-2xl">{richTextItem.title}</h3>
-      )}
+      {richTextItem.title && <h3 className="text-2xl">{richTextItem.title}</h3>}
       <div
         className="prose prose-sm max-w-none space-y-4"
         dangerouslySetInnerHTML={{ __html: richTextItem.text }}
