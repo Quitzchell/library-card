87affa4af803d19d21cefd2dd2b82ba97cd6a678
mitchellquitz <mitchellquitz@gmail.com>
2026-02-20 11:00:25 +0100

    remove ReleaseClient.tsx and ReleaseServer.tsx

--- frontend/components/music/ReleaseServer.tsx
+++
@@ -1,8 +0,0 @@
-import ReleaseClient from "@/components/music/ReleaseClient";
-import { services } from "@/lib/services.config";
-
-export default async function ReleaseServer() {
-  const { data } = await services.music.getReleases();
-
-  return <ReleaseClient items={data} />;
-}
