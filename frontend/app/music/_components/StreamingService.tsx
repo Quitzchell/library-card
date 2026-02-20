916d2b764ce1e2298394a94721fa85396f43cec8
mitchellquitz <mitchellquitz@gmail.com>
2026-02-20 22:44:23 +0100

    clean StreamingService.tsx

--- frontend/app/music/_components/StreamingService.tsx
+++ frontend/app/music/_components/StreamingService.tsx
@@ -1,6 +1,6 @@
 import { ReleaseLink } from "@/lib/interfaces/music";
 import Link from "next/link";
-import { Button } from "../../../components/ui/button";
+import { Button } from "@/components/ui/button";
 
 export default function ReleaseLinks({ link }: { link: ReleaseLink }) {
   return (
